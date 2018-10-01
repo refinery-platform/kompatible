a548654ad2bec6dc2a6049c8f8cd53bb9c377d6c
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-10-01 17:29:22 -0400

    Copy+paste code for kube running

--- kompatible/client.py
+++ kompatible/client.py
@@ -1,3 +1,8 @@
+from kubernetes import client, config
+
+
+config.load_kube_config()
+
 class Client():
 
     @property
@@ -7,4 +12,36 @@ class Client():
 
 class _Containers():
     def run(self, image, command):
+        api = client.CoreV1Api()
+        name = 'TODO_FOOBAR'
+
+        pod_manifest = {
+            'apiVersion': 'v1',
+            'kind': 'Pod',
+            'metadata': {
+                'name': name
+            },
+            'spec': {
+                'containers': [{
+                    'image': image,
+                    'name': name,
+                    "args": [
+                        "/bin/sh",
+                        "-c",
+                        "while true;do date;sleep 5; done"
+                    ]
+                }]
+            }
+        }
+
+        resp = api.create_namespaced_pod(body=pod_manifest,
+                                         namespace='default')
+
+        while True:
+            resp = api.read_namespaced_pod(name=name,
+                                           namespace='default')
+            if resp.status.phase != 'Pending':
+                break
+
+            time.sleep(1)
         pass
